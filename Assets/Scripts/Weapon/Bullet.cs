93677cff25b15539125719003b5cd9b895850436
Thomas <wapps3@gmail.com>
2020-06-04 19:50:20 +0200

    Add bullet

---
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Destroy(gameObject, 5f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 pos = gameObject.transform.position;
+
+        gameObject.transform.position = new Vector3(pos.x + speed * Time.deltaTime, pos.y, pos.z);
+    }
+}
