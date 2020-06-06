b26d8061dce97ddaf7432edf5e96a589c2dd27b4
Exyde <exyde@protonmail.com>
2020-06-06 17:06:32 +0200

    Added Box prefab  + script

---
+++ Assets/Scripts/Props/Box.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Box : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+
+    public float minScale = .2f;
+    public float maxScale = 1f;
+
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+
+        sr.color = new Color(Random.Range(0f, 1f), Random.Range(0f, 1f), Random.Range(0f, 1f));
+        float size = Random.Range(minScale, maxScale);
+        transform.localScale = new Vector3(size, size, size);
+        rb.mass = size;
+    }
+
+}
